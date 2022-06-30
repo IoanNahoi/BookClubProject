f229be36ab68d0d5b694da062d757efa7c938556
inahoi <ioan.nahoi@endava.com>
2022-06-30 14:25:36 +0300

    Initial commit

---
+++ src/main/java/com/endava/tmd/bookclubproject/BookClubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.endava.tmd.bookclubproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookClubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookClubProjectApplication.class, args);
+	}
+
+}
